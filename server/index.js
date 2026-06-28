8f01cb5bb8c9e3bf9f7d8e4ab5f50c810ff8aaa6
Somesh Pandey <someshpandeycil@gmail.com>
2026-06-28 14:36:44 +0530

    fix: open cors for fresh deploy

--- server/index.js
+++ server/index.js
@@ -6,10 +6,7 @@ require('dotenv').config();
 const app = express();
 
 app.use(cors({
-  origin: [
-    'http://localhost:3000',
-    'https://task-tracker-six-sage.vercel.app'
-  ]
+  origin: '*'
 }));
 
 app.use(express.json());
