dc86a48d9871de19335a6571ec9e000a757843f7
Somesh Pandey <someshpandeycil@gmail.com>
2026-06-28 16:41:26 +0530

    Deployment URLs

--- server/index.js
+++ server/index.js
@@ -8,7 +8,7 @@ const app = express();
 app.use(cors({
   origin: [
     'http://localhost:3000',
-    'https://task-tracker-jwmx.vercel.app'  
+    'https://task-tracker-blue-gamma.vercel.app'
   ]
 }));
 
