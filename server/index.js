17ef03005dfa620f9ab5e46c15b019b51849d12b
Somesh Pandey <someshpandeycil@gmail.com>
2026-06-28 14:05:54 +0530

    updated

--- server/index.js
+++ server/index.js
@@ -4,7 +4,14 @@ const cors = require('cors');
 require('dotenv').config();
 
 const app = express();
-app.use(cors());
+
+app.use(cors({
+  origin: [
+    'http://localhost:3000',
+    'https://task-tracker-six-sage.vercel.app'
+  ]
+}));
+
 app.use(express.json());
 
 mongoose.connect(process.env.MONGO_URI)
